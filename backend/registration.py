828d65959e445f7c63449d5b12db26a64d73aff6
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-29 18:22:19 -0700

    add euler angle calculation

--- backend/registration.py
+++ backend/registration.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from collections import namedtuple
+
 
 def _format_points(points_dict):
     return np.array(list(map(
@@ -18,6 +20,8 @@ class Registration:
     _quaternion_rot = None
     _euler_rot = None
     _transformation_mat = None
+    # types
+    EULER_ANGLES = namedtuple('EulerAngles', ['X', 'Y', 'Z'])
 
     def __init__(self, home_pts, other_pts):
         self._home_pts = _format_points(home_pts)
@@ -64,18 +68,31 @@ class Registration:
         u_mat, s_mat, v_mat = np.linalg.svd(h_mat)
 
         # step 5. calculate the rotation matrix
-        self._matrix_rot = np.dot(np.transpose(v_mat), np.transpose(u_mat))
+        rot = np.dot(np.transpose(v_mat), np.transpose(u_mat))
+        self._matrix_rot = rot.tolist()
 
         # step 6. calculate the translation
-        self._translation = centroid_other - np.dot(self._matrix_rot, centroid_home)
+        t = centroid_other - np.dot(rot, centroid_home)
+        self._translation = t.tolist()
 
-        # step 7. calculate quaternion rotation
-        r = np.math.sqrt(1.0 + self._matrix_rot[0, 0] + self._matrix_rot[1, 1] + self._matrix_rot[2, 2]) * 0.5
+        # extra 7.1. set the rotation matrix
+        # print(self._matrix_rot, type(self._matrix_rot), self._translation, type(self._translation))
+
+        # extra 7.2. calculate quaternion rotation
+        r = np.math.sqrt(1.0 + rot[0, 0] + rot[1, 1] + rot[2, 2]) * 0.5
         self._quaternion_rot = [
-            (self._matrix_rot[2, 1] - self._matrix_rot[1, 2]) / (4 * r),  # i
-            (self._matrix_rot[0, 2] - self._matrix_rot[2, 0]) / (4 * r),  # j
-            (self._matrix_rot[1, 0] - self._matrix_rot[0, 1]) / (4 * r),  # k
+            (rot[2, 1] - rot[1, 2]) / (4 * r),  # i
+            (rot[0, 2] - rot[2, 0]) / (4 * r),  # j
+            (rot[1, 0] - rot[0, 1]) / (4 * r),  # k
             r  # real
+        ]
+
+        # extra 7.3. calculate euler angles
+        beta = -np.arcsin(rot[2, 0])
+        self._euler_rot = [
+            np.arctan2(rot[2, 1] / np.cos(beta), rot[2, 2] / np.cos(beta)),  # alpha
+            beta,  # beta
+            np.arctan2(rot[1, 0] / np.cos(beta), rot[0, 0] / np.cos(beta))  # gamma
         ]
 
 
--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,7 @@ def calculate():
     reg = Registration(request.json['home_pts'], request.json['other_pts'])
 
     response = app.response_class(
-        response=json.dumps(reg.matrix_transformation),
+        response=json.dumps(reg.matrix_rot),
         status=200,
         mimetype='application/json'
     )
