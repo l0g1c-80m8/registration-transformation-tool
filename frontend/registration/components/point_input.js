453c22efb02ea404f4e80f6cab8a8753bb3e9dfa
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 15:23:16 -0700

    create input grid for home points

---
+++ frontend/registration/components/point_input.js
@@ -0,0 +1,49 @@
+const PointRow = (prefix, rowNum) => [
+  <div>
+    Point {rowNum}
+  </div>,
+  <div className={"px-2 col-span-2"}>
+    <input
+      className={"block text-black bg-white w-full border border-sky-500 rounded-md focus:outline-none text-center"}
+      type="text"
+      id={`${prefix}_point${rowNum}_x`}
+      name={`${prefix}_point${rowNum}`}
+    />
+  </div>,
+  <div className={"px-2 col-span-2"}>
+    <input
+      className={"block text-black bg-white w-full border border-sky-500 rounded-md focus:outline-none text-center"}
+      type="text"
+      id={`${prefix}_point${rowNum}_y`}
+      name={`${prefix}_point${rowNum}`}
+    />
+  </div>,
+  <div className={"px-2 col-span-2"}>
+    <input
+      className={"block text-black bg-white w-full border border-sky-500 rounded-md focus:outline-none text-center"}
+      type="text"
+      id={`${prefix}_point${rowNum}_z`}
+      name={`${prefix}_point${rowNum}`}
+    />
+  </div>
+]
+
+export default function PointInput({ title }) {
+  return <div className={"w-full"}>
+    <div className={"text-xl pt-2"}>
+      {title}
+    </div>
+    <div className={"grid gap-4 grid-cols-7 grid-rows-5 my-4 text-center"}>
+      <div>Point #</div>
+      <div className={"col-span-2"}>X</div>
+      <div className={"col-span-2"}>Y</div>
+      <div className={"col-span-2"}>Z</div>
+
+      {...PointRow(title.toLowerCase(), 1)}
+      {...PointRow(title.toLowerCase(), 2)}
+      {...PointRow(title.toLowerCase(), 3)}
+      {...PointRow(title.toLowerCase(), 4)}
+    </div>
+    <div className={"py-2"}> Upload File</div>
+  </div>
+}
--- frontend/registration/pages/index.js
+++ frontend/registration/pages/index.js
@@ -1,19 +1,22 @@
+import PointInput from "@/components/point_input";
+
 export default function Home() {
   return (
     <main className={"min-h-screen min-w-screen dark:bg-slate-800 text-zinc-50"}>
-      <div className={"text-4xl pl-6 pt-6"}>
+      <div className={"text-5xl pl-6 pt-6"}>
         Registration Tool
       </div>
       <hr className="h-px mx-2 my-6 bg-gray-100 border-0 rounded dark:bg-gray-100"/>
       <div className={"flex flex-row w-full"}>
         <div className={"bg-sky-200 rounded-lg w-full mx-3 p-3 text-sky-800"}>
-          <div className={"text-2xl"}>
+          <div className={"text-3xl"}>
             Input
           </div>
           <hr className="h-px my-1 bg-gray-200 border-0 dark:bg-sky-800"/>
+          <PointInput title={"Home Points"}></PointInput>
         </div>
         <div className={"bg-sky-200 rounded-lg w-full mx-3 p-3 text-sky-800"}>
-          <div className={"text-2xl"}>
+          <div className={"text-3xl"}>
             Output
           </div>
           <hr className="h-px my-1 bg-gray-200 border-0 dark:bg-sky-800"/>
