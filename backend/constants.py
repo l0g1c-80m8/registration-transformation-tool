8ec4c196542c26c96727c05dce00dfbd68442c37
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-29 18:34:43 -0700

    use pre-defined keys for points

--- backend/constants.py
+++ backend/constants.py
@@ -4,3 +4,8 @@ MODES = Namespace(
     FLUSH='FLUSH',
     DEFERRED='DEFERRED'
 )
+
+INPUT_KEYS = Namespace(
+    HOME_FRAME_POINTS='home_pts',
+    OTHER_FRAME_POINTS='other_pts'
+)
--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,17 @@
 from flask import *
 from registration import Registration
-from constants import MODES
+from constants import INPUT_KEYS, MODES
 
 app = Flask(__name__, static_folder="./")
 
 
 @app.route("/calculate", methods=["POST"])
 def calculate():
-    reg = Registration(request.json['home_pts'], request.json['other_pts'], MODES.FLUSH)
+    reg = Registration(
+        request.json[INPUT_KEYS.HOME_FRAME_POINTS],
+        request.json[INPUT_KEYS.OTHER_FRAME_POINTS],
+        MODES.FLUSH
+    )
 
     response = app.response_class(
         response=json.dumps(reg.transformation_mat),
