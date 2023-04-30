794bbc48fa388c9ec425f4e9784609f7cb0cf32a
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 00:51:08 -0700

    updated styles to be at the component level

--- frontend/registration/pages/index.js
+++ frontend/registration/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className={"min-h-screen min-w-screen dark:bg-slate-800 text-zinc-50 text-4xl pl-6 pt-6"}>
-      <div>
+    <main className={"min-h-screen min-w-screen dark:bg-slate-800"}>
+      <div className={"text-zinc-50 text-4xl pl-6 pt-6"}>
         Registration Tool
       </div>
     </main>
