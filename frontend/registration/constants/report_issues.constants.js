a71352411ccf2e8b5bd25902c439b42da6b15eb8
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 16:53:42 -0700

    update report instructions

--- frontend/registration/constants/report_issues.constants.js
+++ frontend/registration/constants/report_issues.constants.js
@@ -1,6 +1,7 @@
 const REPORT_INSTRUCTIONS = [
-  "If you find any issues with this tool, you can report them on" +
-  " <a target='_blank' href='https://github.com/l0g1c-80m8/registration-transformation-tool/issues'>GitHub</a>.",
+  "If you find any issues with this tool, you can report them on " +
+  "<a target='_blank' href='https://github.com/l0g1c-80m8/registration-transformation-tool/issues'>GitHub 🔗</a>.",
+  "Visit the issue creation page by clicking the link icon above.",
   "Create a new issue and describe the problem in as much detail as possible.",
   "List all the steps required to reproduce the issue.",
   "Attach screenshot, POST API response, errors in the console, or any other resource that would be helpful."
