380ff599a900077172cb6c9b3c9ba89e34eaba44
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 12:59:34 -0700

    update border styles

--- frontend/registration/pages/index.js
+++ frontend/registration/pages/index.js
@@ -4,17 +4,20 @@ export default function Home() {
       <div className={"text-4xl pl-6 pt-6"}>
         Registration Tool
       </div>
-      <hr className="h-px my-6 bg-gray-50 border-0 dark:bg-gray-50"/>
+      <hr className="h-px mx-2 my-6 bg-gray-100 border-0 rounded dark:bg-gray-100"/>
       <div className={"flex flex-row w-full"}>
         <div className={"bg-sky-200 rounded-lg w-full mx-3 p-3 text-sky-800"}>
           <div className={"text-2xl"}>
             Input
           </div>
+          <hr className="h-px my-1 bg-gray-200 border-0 dark:bg-sky-800"/>
+
         </div>
         <div className={"bg-sky-200 rounded-lg w-full mx-3 p-3 text-sky-800"}>
           <div className={"text-2xl"}>
             Output
           </div>
+          <hr className="h-px my-1 bg-gray-200 border-0 dark:bg-sky-800"/>
         </div>
       </div>
       <div>
