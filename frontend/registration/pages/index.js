5fc63140175190458d98e6e92145d28cb1356c82
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 00:49:08 -0700

    remove unused import and consts

--- frontend/registration/pages/index.js
+++ frontend/registration/pages/index.js
@@ -1,8 +1,3 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main className={"min-h-screen min-w-screen dark:bg-slate-800 text-zinc-50 text-4xl pl-6 pt-6"}>
