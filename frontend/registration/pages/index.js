8b5bcb94986b38d2b51303c293921c3d69f8ea3a
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 12:49:43 -0700

    create input and output sections

--- frontend/registration/pages/index.js
+++ frontend/registration/pages/index.js
@@ -1,8 +1,24 @@
 export default function Home() {
   return (
-    <main className={"min-h-screen min-w-screen dark:bg-slate-800"}>
-      <div className={"text-zinc-50 text-4xl pl-6 pt-6"}>
+    <main className={"min-h-screen min-w-screen dark:bg-slate-800 text-zinc-50"}>
+      <div className={"text-4xl pl-6 pt-6"}>
         Registration Tool
+      </div>
+      <hr className="h-px my-6 bg-gray-50 border-0 dark:bg-gray-50"/>
+      <div className={"flex flex-row w-full"}>
+        <div className={"bg-sky-200 rounded-lg w-full mx-3 p-3 text-sky-800"}>
+          <div className={"text-2xl"}>
+            Input
+          </div>
+        </div>
+        <div className={"bg-sky-200 rounded-lg w-full mx-3 p-3 text-sky-800"}>
+          <div className={"text-2xl"}>
+            Output
+          </div>
+        </div>
+      </div>
+      <div>
+        Section with usage instructions and contribution/error reporting instructions
       </div>
     </main>
   )
