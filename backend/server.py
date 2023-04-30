3d01ebd18878e725592afa5fa9481ec8b1433396
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-29 17:52:52 -0700

    add server file

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import *
+from registration import Registration
+
+app = Flask(__name__, static_folder="./")
+
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    reg = Registration(request.json['home_pts'], request.json['other_pts'])
+
+    response = app.response_class(
+        response=json.dumps(reg.matrix_transformation),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
