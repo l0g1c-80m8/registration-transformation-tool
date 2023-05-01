6e4e1639a73bff26312d91b207c05ec91de9e0bb
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 20:48:55 -0700

    add actions and constants started code

---
+++ frontend/registration/constants/constants.js
@@ -0,0 +1,22 @@
+import _noop from 'lodash/noop';
+
+import { INPUT_ACTIONS } from "@/actions/actions";
+
+const PAGE_ACTIONS = {
+  [INPUT_ACTIONS.SUBMIT]: () => {
+    console.log('submit');
+  },
+  [INPUT_ACTIONS.CLEAR]: () => {
+    console.log('clear');
+  },
+  [INPUT_ACTIONS.POINT_INPUT]: id => {
+    _noop()
+  },
+  [INPUT_ACTIONS.FILE_SELECT]: id => {
+    console.log(`file select ${id}`);
+  },
+};
+
+export {
+  PAGE_ACTIONS
+};
