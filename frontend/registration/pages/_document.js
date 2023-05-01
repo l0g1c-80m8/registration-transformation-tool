b14e5849473bb8176e145f3009d0ba25a8c8a5b1
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-30 17:06:15 -0700

    add logo to the page

--- frontend/registration/pages/_document.js
+++ frontend/registration/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="logo.gif" type="image/gif" />
+      </Head>
       <body>
         <Main />
         <NextScript />
