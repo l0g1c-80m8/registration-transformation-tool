d7fe7e0845139d0a62f844e64a60f960d0b25871
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-05-08 12:52:54 -0700

    update cors policy for backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from flask import *
+from flask_cors import cross_origin
 from registration import Registration
+
 from constants import APP_NAME, MODES, REQ_KEYS
 
 app = Flask(APP_NAME, static_folder="./")
 
 
 @app.route("/calculate", methods=["POST"])
+@cross_origin()
 def calculate():
     reg = Registration(
         request.json[REQ_KEYS.HOME_FRAME_POINTS],
