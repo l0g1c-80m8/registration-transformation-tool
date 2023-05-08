4b7fcb19816c8596d1062146253ff282eebdabf2
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-05-07 18:50:56 -0700

    handle clear action

--- frontend/registration/actions/actions.js
+++ frontend/registration/actions/actions.js
@@ -1,11 +1,22 @@
+import _forEach from 'lodash/forEach';
+import _keysIn from 'lodash/keysIn';
+import _set from 'lodash/set';
+
 import { INPUT_ACTIONS } from "@/constants/constants";
 
 const getPageActions = (pageState, setPageState) => ({
   [INPUT_ACTIONS.SUBMIT]: () => {
+    // read state to get all points
+    // validate that the values are correct
+    // send a post request to be
+    // set the state for output section
     console.log('submit', pageState);
   },
   [INPUT_ACTIONS.CLEAR]: () => {
-    setPageState({});
+    // set all keys to empty string values
+    const copyState = { ...pageState };
+    _forEach(_keysIn(pageState), key => _set(copyState[key] = ''));
+    setPageState({ ...copyState });
   },
   [INPUT_ACTIONS.POINT_INPUT]: id => event => {
     setPageState({
