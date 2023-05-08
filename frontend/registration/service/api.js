2154aea58a16a0be94c3ef22d7e2fc2e30d0fd1e
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-05-08 15:57:43 -0700

    update api for the service instance with headers

--- frontend/registration/service/api.js
+++ frontend/registration/service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const registrationService = axios.create({
+  headers: { 'Content-Type': 'application/json' },
   baseURL: 'https://registration-server-backend.wl.r.appspot.com',
   timeout: 300,
 });
