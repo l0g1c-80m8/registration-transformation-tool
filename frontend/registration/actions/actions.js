2cbdcda90165e56cee154d09accf8ed3c69e8f4e
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-05-08 12:57:15 -0700

    convert string to number before making the api request call

--- frontend/registration/actions/actions.js
+++ frontend/registration/actions/actions.js
@@ -1,12 +1,70 @@
-import { INITIAL_STATE, INPUT_ACTIONS } from "@/constants/constants";
+import _get from 'lodash/get';
+import _toNumber from 'lodash/toNumber';
+
+import { fetchTransformation } from "@/service/api";
+
+import {
+  INITIAL_STATE,
+  INPUT_ACTIONS,
+  HOME_POINTS_ID_PREFIX,
+  OTHER_POINTS_ID_PREFIX,
+  OUTPUT_SECTION_ID
+} from "@/constants/constants";
 
 const getPageActions = (pageState, setPageState) => ({
-  [INPUT_ACTIONS.SUBMIT]: () => {
-    // read state to get all points
+  [INPUT_ACTIONS.SUBMIT]: async () => {
+    const payload = {
+      [HOME_POINTS_ID_PREFIX]: {
+        'pt1': [
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_0_x`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_0_y`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_0_z`, null))
+        ],
+        'pt2': [
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_1_x`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_1_y`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_1_z`, null))
+        ],
+        'pt3': [
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_2_x`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_2_y`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_2_z`, null))
+        ],
+        'pt4': [
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_3_x`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_3_y`, null)),
+          _toNumber(_get(pageState, `${HOME_POINTS_ID_PREFIX}_4_z`, null))
+        ],
+      },
+      [OTHER_POINTS_ID_PREFIX]: {
+        'pt1': [
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_0_x`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_0_y`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_0_z`, null))
+        ],
+        'pt2': [
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_1_x`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_1_y`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_1_z`, null))
+        ],
+        'pt3': [
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_2_x`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_2_y`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_2_z`, null))
+        ],
+        'pt4': [
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_3_x`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_3_y`, null)),
+          _toNumber(_get(pageState, `${OTHER_POINTS_ID_PREFIX}_4_z`, null))
+        ],
+      }
+    };
     // validate that the values are correct
-    // send a post request to be
-    // set the state for output section
-    console.log('submit', pageState);
+    const response = await fetchTransformation(payload);
+    setPageState({
+      ...pageState,
+      [OUTPUT_SECTION_ID]: response
+    })
   },
   [INPUT_ACTIONS.CLEAR]: () => {
     // set all keys to empty string values
