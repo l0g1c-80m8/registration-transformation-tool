9b1a87419755c34dadd959fcaf9114b277e411f0
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-05-08 12:32:03 -0700

    make text area read only

--- frontend/registration/components/output.js
+++ frontend/registration/components/output.js
@@ -1,5 +1,10 @@
 export default function Output({ value }) {
   return <div className="p-2">
-    <textarea id="output" className="w-full h-full" value={value}/>
+    <textarea
+      id="output" className="w-full h-full focus:outline-none"
+      value={value}
+      aria-readonly
+      readOnly
+    />
   </div>
 }
