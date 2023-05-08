d0423b49da2bc723c212f9e7278dfdbe7b0f3e4b
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-05-07 18:56:48 -0700

    update instructions

--- frontend/registration/constants/instructions.constants.js
+++ frontend/registration/constants/instructions.constants.js
@@ -1,8 +1,10 @@
 const INSTRUCTIONS = [
   "Enter four points for the home and other coordinate frames of references by entering the values in the text " +
   "fields or by selecting a text or csv file with the correct order (x, y, z).",
-  "After all four fields have been populated, wait for the system to generate the transformation.",
-  "The generated transformation will be available under the output section."
+  "After all four fields have been populated, click on the submit button and wait for the system to generate the " +
+  "transformation.",
+  "The generated transformation will be available under the output section.",
+  "Click on the clear button to clear all the fields to start over."
 ];
 
 export default INSTRUCTIONS
