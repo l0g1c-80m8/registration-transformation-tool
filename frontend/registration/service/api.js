510f21134ff43f092568e78d10d1e5ff93218151
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-05-08 14:05:26 -0700

    update base URL to remote URL

--- frontend/registration/service/api.js
+++ frontend/registration/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const registrationService = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://registration-server-backend.wl.r.appspot.com',
   timeout: 300,
 });
 
